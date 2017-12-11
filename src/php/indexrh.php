da62c44c50674a63e69d88e976a4867a1773f482
Amaury <amaury.deguilhem@gmail.com>
2017-12-11 17:22:36 +0100

    BONSWAG

--- src/php/indexrh.php
+++ src/php/indexrh.php
@@ -31,11 +31,60 @@ $page = explode("/", pagencours);
         </div>
     </div>
 </nav>
-
-<script>
-
-</script>
-
+<br/>
+<div class="container-fluid">
+    <table class="table table-hover table-bordered">
+        <thead>
+        <tr>
+            <th>~</th>
+            <th>NbJours</th>
+            <th>Début</th>
+            <th>Fin</th>
+            <th>Nom</th>
+            <th>Poste</th>
+            <th>Email</th>
+            <th>Jours pris</th>
+            <th colspan="2"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>3</td>
+            <td>21/01/2018</td>
+            <td>24/01/2018</td>
+            <td>Jean-Charles</td>
+            <td>Ouvrier</td>
+            <td>jean-charles@gmail.com</td>
+            <td>15</td>
+            <td colspan="2" style="background-color: #5cb85c"></td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>1</td>
+            <td>14/02/2018</td>
+            <td>14/02/2018</td>
+            <td>Jean-Eudes</td>
+            <td>Chef de projet</td>
+            <td>jean-eudes@gmail.com</td>
+            <td>9</td>
+            <td><button type="button" class="btn btn-success">Accepter</button></td>
+            <td><button type="button" class="btn btn-danger">Refuser</button></td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>2</td>
+            <td>25/07/2018</td>
+            <td>27/07/2018</td>
+            <td>Jean-Luc</td>
+            <td>Fonction support</td>
+            <td>jean-luc@gmail.com</td>
+            <td>33</td>
+            <td colspan="2" style="background-color: #d9534f"></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 <script src="../BootStrap/js/bootstrap.min.js"></script>
 </body>
 
